d2a6b8c8acf5674ea52168f6e243b8a5ec357b83
Saaim Japanwala <92124191+sjapanwala@users.noreply.github.com>
2024-03-17 01:58:20 -0600

    Update and rename main.cmd to netwise.cmd

--- netwise.cmd
+++ netwise.cmd
@@ -35,17 +35,27 @@ set displaynotifs=True
 ::display func
 set targetipdisplay=%darkred%%targetip%%brightwhite%
 set iptypedisplay=%brightgreen%%iptype%%brightwhite%
+set targetdir=C:\Users\%username%
 ::curl vars
-FOR /F %%a IN ('curl https://ipv4.icanhazip.com/') DO set localip=%%a
+FOR /F %%a IN ('curl-s https://ipv4.icanhazip.com/') DO set localip=%%a
 :startup
 cls
 if not %displaynotifs%==True goto commandline
-if exist warns\.install_file echo %white%🪧 Please Install Files For Full Access
+if not %cd% == %targetdir% goto plantnoti
+goto commandline
+:plantnoti
+echo ┌─(%darkblue%Please Plant This File Into %brightgreen%%targetdir%%white% For Full Functionality)
+echo │
+echo ├─'%white%Use "%brightgreen%root-plant%white%" as root user to plant this file'
+echo └─'%white%Plant this file to not show this message again, or type "%brightgreen%disable-notif%white%" to temporarily disable this message'
 :commandline
 echo %brightwhite%
 echo ┌──(%brightred%%termusername%%brightwhite%@%brightred%%computername%%brightwhite%)-[%iptype%][%brightpurple%%targetip%%brightwhite%]
 set /p command=└─%brightred%!%brightwhite%
 :: general commands
+if "%command%"=="docu" goto docu
+if "%command%"=="exit" exit
+if "%command%"=="readme" start https://shorturl.at/uAIMO && goto commandline
 if "%command%"=="help" goto help
 if "%command%"=="help-a" goto allhelp
 if "%command%"=="cls" goto startup
@@ -78,6 +88,17 @@ if "%command%"=="test-h" goto testhelp
 if "%command%"=="test-net" goto testnet
 
 
+:: disable
+if "%command%"=="disable-h" goto disablehelp
+if "%command%"=="disable-notif" goto disablenotif
+
+
+::generateip
+if "%command%"=="genip-h" goto geniphelp
+if "%command%"=="genip-create" goto genip
+if "%command%"=="genip-loop" goto geniploop
+
+
 ::geo
 
 ::whois
@@ -89,12 +110,13 @@ if "%command%"=="tracepath-web" goto connectpathweb
 
 ::matrix
 
+::
+
 goto invalidcommand
 
 :invalidcommand
 echo.
-echo %white%Command '%command%' not found, Try help or (command)-h
-echo.
+echo %white%Command '%command%' not found, Try help or Try "%command%-h"
 goto commandline
 
 
@@ -103,17 +125,32 @@ echo.
 echo ╔General Help
 echo ╚:Shows general help
 echo.
+echo docu :--: shows documentation
+echo readme :: redirects to the program github/readme
 echo help :--: shows this page
 echo help-a :: shows all help
 echo cls :---: clears screen
 echo plant :-: plants file into C:\Users\%username%
 echo swusr :-: switches user
 echo info :--: shows information
+echo exit :--: exits a program
 echo. 
 echo For Command Help, Type "(commandname)-h"
 echo.
 goto commandline
 
+:docu
+echo.
+echo %brightwhite%Welcome to %brightred%NetWise%brightwhite%
+echo.
+echo - ReadMe File (https://shorturl.at/uAIMO)
+echo %white%Note: if link cannot be clicked, type "%brightgreen%readme%brightwhite%" to access readme
+echo.
+echo Summary,
+echo NetWise is a terminal based program that utilizes API's, Windows Commands and, many self made programs that
+echo can assist with whatever network aid you desire! Commands can be accessed from the ReadMe File from github ("%brightgreen%readme%brightwhite%")
+echo or type "%white%(commandname)-h%brightwhite%" for help with a command, if you already know the command but need help with parameters.
+goto commandline
 
 :switchuser
 echo.
@@ -147,13 +184,35 @@ set termusername=root
 echo %brightgreen%Successfully Logged Into Root%brightwhite%
 echo.
 goto commandline
+
 :roothelp
-if not %userpermission%==root echo %brightred%User Permission Is Not Allowed%brightwhite% && goto commandline
 echo.
 echo ╔Root User Help
-echo ╚:Mostly Config Commands
+echo ╚:Root Access Is REQUIRED
+echo.
+echo root-h :----: access this help menu
+echo root-plant :: plant this file into %targetdir%
+echo root-log :--: access log files
 echo.
 goto commandline
+
+:plant
+if not %userpermission%==root echo %brightred%User Permission Is Not Allowed%brightwhite% && goto commandline
+move netwise.cmd C:\Users\%username%
+
+:disablehelp
+echo.
+echo ╔Disable Help
+echo ╚:Disable Items, Root Access Is REQUIRED for some operations
+echo.
+echo disable-h :----: shows this help menu
+echo disable-notif :----: disables notifications temporarily
+echo.
+goto commandline
+
+:disablenotif
+if %displaynotifs%==True set displaynotifs=False && echo %brightgreen%Notifications Disabled For Current Session, %white%"%brightgreen%cls%white%" For Changes to Take Place && goto commandline
+if %displaynotifs%==False echo %brightyellow%Notifications Are Already Disabled && goto commandline
 
 
 :info
@@ -178,9 +237,6 @@ echo %brightwhite%Host OS :-----------: %darkred%%OS%
 echo.
 goto commandline
 
-:plant
-if not %userpermission%==root echo %brightred%User Permission Is Not Allowed%brightwhite% && goto commandline
-
 :inserthelp
 echo.
 echo ╔Insert Help
@@ -193,8 +249,10 @@ echo.
 goto commandline
 :insertip
 echo.
+echo %white%Note: Enter "%brightgreen%ip-l%white%" To Enter Local IP (%localip%)
 echo ┌(%brightred%%targetip%%brightwhite%)
 set /p newip=└[Enter New IP]:
+if "%newip%"=="ip-l" set newip=%localip%
 echo ╔═══════════════IP Connection═════════════════╗
 curl http://ip-api.com/line/%newip%?fields=query
 curl http://ip-api.com/line/%newip%?fields=status
@@ -202,12 +260,14 @@ curl http://ip-api.com/line/%newip%?fields=message
 curl http://ip-api.com/line/%newip%?fields=org
 echo ╚═════════════════════════════════════════════╝
 echo.
+:setchoiceip
 if %newip%==%safetydomain% echo %brightyellow%⚠️!: TEMP-TARGET IP AND SAFETY DOMAIN MATCH %brightwhite%
 set /p choice=Set %newip% as Target IP? (Y/N): 
 if %choice%==y goto settarget
 if %choice%==Y goto settarget
 if %choice%==N echo %brightyellow%IP Set Abborted && goto commandline
 if %choice%==n echo %brightyellow%IP Set Abborted && goto commandline
+goto setchoiceip
 :settarget
 set targetip=%newip%
 echo %brightgreen%%newip% Has Been Set As New Target%white%
@@ -253,14 +313,14 @@ echo.
 curl http://ip-api.com/?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,mobile,proxy,hosting,query
 echo.
 goto commandline
-:ipdisplaytarget2
+:ipdisplaytarget
 echo.
 if %targetip%==none echo %brightred% No IP Set, Please Set A Target IP First%brightwhite% && goto commandline
 echo %brightcyan%
-curl http://ip-api.com/line/?fields=%targetip%,status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query
+curl ip-api.com/%targetip%
 echo.
 goto commandline
-:ipdisplaytarget
+:ipdisplaytarget2
 if %targetip%==none echo %brightred% No IP Set, Please Set A Target IP First%brightwhite% && goto commandline
 echo.
 echo %white% Note: this branch may not provide stable information, if you get an error, please try %brightgreen%"ipdisplay-t"%white%
@@ -393,3 +453,74 @@ echo.
 echo.
 PING -n 1 %net% | FIND "TTL=">nul && echo %brightwhite%Connection: %brightgreen%Great&& goto commandline
 IF ERRORLEVEL 1 (SET in=0b & echo %brightwhite%Connection: %brightred%No Network Connected/No Signals) &&  goto commandline
+
+
+:geniphelp
+echo.
+echo ╔Generate Random IP
+echo ╚:Combines random numbers to generate a possible IP
+echo.
+echo genip-h :-----: shows this help menu
+echo genip-create :: creates a singular random IP, shows whether said IP exists
+echo genip-loop :--: creates multiples IP until a real IP is found (depends on API key, and Max Requests)
+goto commandline
+:genip
+echo.
+set /a num=%random% %%4
+set /a num1=%random% %%4
+set /a num2=%random% %%4
+set /a num3=%random% %%4
+set /a num4=%random% %%4
+set /a num5=%random% %%4
+set /a num6=%random% %%4
+set /a num7=%random% %%4
+set /a num8=%random% %%4
+set /a num9=%random% %%4
+set /a num10=%random% %%4
+set /a num11=%random% %%4
+echo.
+echo %brightgreen%Random IP Generated
+echo %white%Note: This is a completely random generated IP. If connected to something important, its an entire coincident.
+set generatedip=%num%%num1%%num2%.%num3%%num4%%num5%.%num6%%num7%%num8%.%num9%%num10%%num11%
+echo.
+echo Randomly Generated IP: %generatedip%
+curl http://ip-api.com/line/%generatedip%?fields=status
+goto commandline
+:geniploop
+echo %white%Note: This Will Run Until A Valid IP is found
+echo.
+:startloop
+set /a num=%random% %%4
+set /a num1=%random% %%4
+set /a num2=%random% %%4
+set /a num3=%random% %%4
+set /a num4=%random% %%4
+set /a num5=%random% %%4
+set /a num6=%random% %%4
+set /a num7=%random% %%4
+set /a num8=%random% %%4
+set /a num9=%random% %%4
+set /a num10=%random% %%4
+set /a num11=%random% %%4
+set generatedip=%num%%num1%%num2%.%num3%%num4%%num5%.%num6%%num7%%num8%.%num9%%num10%%num11%
+for /f "tokens=*" %%i in ('curl -s "http://ip-api.com/line/%generatedip%?fields=status"') do set status=%%i
+echo %brightred%%status%
+echo %generatedip%
+PING -n 1 8.8.8.8 | FIND "TTL=">nul
+if %status%==fail goto startloop
+echo.
+echo %brightwhite%Status: %brightgreen%[%status%]
+echo %brightwhite%[%generatedip%] Was Successfully Found
+echo.
+:settargetrand
+set /p choice=Set %generatedip% As TargetIP? (Y/N): 
+if %choice%==y goto settargettrue
+if %choice%==Y goto settargettrue
+if %choice%==N echo %brightyellow%IP Set Abborted && goto commandline
+if %choice%==n echo %brightyellow%IP Set Abborted && goto commandline
+goto settargetrand
+:settargettrue
+set targetip=%generatedip%
+echo %brightgreen%%generatedip% Has Been Set As New Target%white%
+goto commandline
+
